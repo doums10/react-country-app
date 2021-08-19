c3f1272091dbe016b6d9a73b840c955df0fa031c
doums10 <m27sylla@gmail.com>
2021-08-18 23:30:34 -0400

    affichage countries sur page home

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Countries = () => {
-  const [data, setData] = useState("Hello");
+  const [data, setData] = useState([]);
 
-  return <div></div>;
+  useEffect(() => {
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag%22"
+      )
+      .then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <div className="countries">
+      <ul className="countries-list">
+        {data.map((country) => (
+          <li>{country.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Countries;
