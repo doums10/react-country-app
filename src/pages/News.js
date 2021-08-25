f51182eb05ddd6b6aba16cce7593bacb4f4ab42c
doums10 <m27sylla@gmail.com>
2021-08-24 20:21:03 -0400

    ajout des author et du content dans le form dans la db

--- src/pages/News.js
+++ src/pages/News.js
@@ -5,8 +5,10 @@ import axios from "axios";
 import Article from "../components/Article";
 
 const News = () => {
+  const [newsData, setNewsData] = useState([]);
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
 
-	const [newsData, setNewsData] = useState([]);
   useEffect(() => {
     getData();
   }, []);
@@ -17,20 +19,47 @@ const News = () => {
       .then((res) => setNewsData(res.data));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:3003/articles", {
+      author,
+      content,
+      date: Date.now(),
+    })
+    .then(() => {
+    setAuthor("");
+    setContent("");
+    });
+  };
+
   return (
     <div className="news-container">
       <Navigation />
       <Logo />
       <h1>News</h1>
 
-      <form>
-        <input type="text" placeholder="Nom" />
-        <textarea placeholder="Message"></textarea>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          onChange={(e) => setAuthor(e.target.value)}
+          type="text"
+          placeholder="Nom"
+          value={author}
+        />
+        <textarea
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Message"
+          value={content}
+        ></textarea>
         <input type="submit" value="Envoyer" />
       </form>
-      <ul>{newsData.map((article) => (
-							<Article key={article.id} article={article}/>
-						))}</ul>
+      <ul>
+        {newsData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
